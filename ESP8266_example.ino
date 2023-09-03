711628b298492ce5b21643d719a96199c3c25d7d
Akhsarbek Gozoev <gakhsar@gmail.com>
2023-09-03 14:01:54 +0200

    update esp8266 example

--- ESP8266_example.ino
+++ ESP8266_example.ino
@@ -1,6 +1,8 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <LittleFS.h>
-#include <ota.h>
+#include <GitHubOTA.h>
+#include <GitHubFsOTA.h>
 
 // This string should correspond to github tag used for Releasing (via. Github Actions)
 #define VERSION "0.0.1"
@@ -10,7 +12,7 @@
 // #define RELEASE_URL "https://api.github.com/repos/your_username/your_repo/releases/latest"
 
 // Use this version of the URL together with 
-//Line 22: GitHubOTA GitHubOTA(VERSION, RELEASE_URL, "firmware.bin", "filesystem.bin", "/.fs_update_pending", false);
+// Line 22: GitHubOTA GitHubOTA(VERSION, RELEASE_URL, "firmware.bin", "filesystem.bin", "/.fs_update_pending", false);
 // under debugging to spare yourself from getting timeout from GitHub API
 #define RELEASE_URL "https://github.com/your_username/your_repo/releases/latest"
 
@@ -22,7 +24,39 @@
 // Uncomment the line below when using Github Api to get the latest release (Line 9)
 //GitHubOTA GitHubOTA(VERSION, RELEASE_URL);
 
-GitHubOTA GitHubOTA(VERSION, RELEASE_URL, "firmware.bin", "filesystem.bin", "/.fs_update_pending", false);
+GitHubOTA OsOta(VERSION, RELEASE_URL, "firmware.bin", true);
+GitHubFsOTA FsOta(VERSION, RELEASE_URL, "filesystem.bin", true);
+
+void setup_wifi();
+void listRoot();
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  while(!Serial);
+
+  Serial.printf("Current firmware version: %s\n", VERSION); 
+  Serial.printf("Current filesystem version: %s\n", VERSION); 
+
+  LittleFS.begin();
+  listRoot();
+
+  setup_wifi();
+
+  // Chech for updates
+  FsOta.handle();
+  OsOta.handle();
+}
+
+void loop()
+{
+  // Your code goes here
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(DELAY_MS);                  // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(DELAY_MS);                  // wait for a second
+}
 
 void listRoot(){
   Serial.printf("Listing root directory\r\n");
@@ -34,34 +68,6 @@ void listRoot(){
     Serial.printf("  FILE: %s\r\n", file.name());
     file = root.openNextFile();
   }
-}
-
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  while(!Serial);
-
-  Serial.printf("Current firmware version: %s\n", VERSION); 
-
-  LittleFS.begin();
-  listRoot();
-
-  setup_wifi();
-
-  GitHubOTA.handle();
-}
-
-void loop()
-{
-  // Uncomment this line to check for updates on every loop iteration
-  // GitHubOTA.handle();
-
-  // Your code goes here
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(DELAY_MS);                  // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(DELAY_MS);                  // wait for a second
 }
 
 void setup_wifi(){
